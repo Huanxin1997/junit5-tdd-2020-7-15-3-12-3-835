f492d63737ddec4afb8c1bb47b5a5d841d3485e8
Hans <741581879@qq.com>
2020-07-23 11:55:25 +0800

    [Hans] feature: Implete return fizz when play fizz buzz given 3

--- src/main/java/junit5/tdd/FizzBuzz.java
+++ src/main/java/junit5/tdd/FizzBuzz.java
@@ -2,6 +2,9 @@ package junit5.tdd;
 
 public class FizzBuzz {
     public String play(int i) {
+        if(i % 3 == 0) {
+            return "Fizz";
+        }
         return String.valueOf(i);
     }
 }
